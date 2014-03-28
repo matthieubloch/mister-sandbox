ce3c100897ceaf69b90f026d3047716a14515756
Kuya <matthieu.k.bloch@gmail.com>
2014-03-28 08:54:51 +0100

    Yay, heritage

--- src/SortOf.java
+++ src/SortOf.java
@@ -1,7 +1,17 @@
 
 public class SortOf {
 	
+	private String what;
+	
+	public SortOf(){
+		what = "class";
+	}
+	
+	public void setWhat(String what){
+		this.what = what;
+	}
+	
 	public String toString(){
-		return "I'm a sort of class";
+		return "I'm a sort of " + what;
 	}
 }
--- src/Woodpecker.java
+++ src/Woodpecker.java
@@ -1,6 +1,12 @@
 
 public class Woodpecker extends SortOf {
+	
+	public Woodpecker(){
+		super();
+		setWhat("bird");
+	}
+	
 	public String toString(){
-		return super.toString() + " Thing"; 
+		return super.toString(); 
 	}
 }
